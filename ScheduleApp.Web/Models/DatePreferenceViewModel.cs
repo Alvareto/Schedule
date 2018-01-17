aef10fcb28561ea26b49486d2700ddbc08d905dc
Ivan Grgurina <ivan.grgurina@gmail.com>
2018-01-17 18:54:53 +0100

    Changed views to be more clear and added few design tweaks and features

--- ScheduleApp.Web/Models/DatePreferenceViewModel.cs
+++ ScheduleApp.Web/Models/DatePreferenceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace ScheduleApp.Web.Models
 {
     public class DatePreferenceViewModel
     {
+        [Display(Name="Date")]
         public DateTime Day { get; set; } // ShiftId
+        [Display(Name = "Is Preffered?")]
         public bool IsPreffered { get; set; }
     }
 }
